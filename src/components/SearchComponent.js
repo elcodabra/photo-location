050a15c1cd17d0d60405085179cda50ca3a52d17
Alex Konovalov <akonovalov@at-consulting.ru>
2015-12-11 19:11:01 +0300

    add instagram query

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,13 +1,27 @@
 'use strict';
 
 import React from 'react';
+import * as $ from 'jquery';
 
 require('styles//Search.less');
 
 class SearchComponent extends React.Component {
   render() {
     var onSearch = function() {
-      console.log("Search...");
+      $.ajax({
+        url: "https://api.instagram.com/v1/media/search?lat=48.858093&lng=2.294694&client_id=e050a30d1667451cbc3598f3cce20530",
+
+        // The name of the callback parameter, as specified by the YQL service
+        jsonp: "callback",
+
+        // Tell jQuery we're expecting JSONP
+        dataType: "jsonp",
+
+        // Work with the response
+        success: function( response ) {
+          console.log( response ); // server response
+        }
+      });
     };
     return (
       <div className="search-component">
