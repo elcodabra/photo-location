34f0638d431727dffea32436bee33bfd77790cd1
akonovalov <akonovalov@at-consulting.ru>
2015-12-15 23:56:01 +0300

    add autosuggest component

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import Actions from '../actions/Action';
 import appStore from '../stores/AppStore';
+import Autosuggest from 'react-autosuggest';
 import * as $ from 'jquery';
 
 require('styles//Search.less');
@@ -27,15 +28,39 @@ class SearchComponent extends React.Component {
   render() {
     let places = this.state.places;
     console.log(places);
-    var onSearch = function() {
+    let onSearch = function() {
       //Actions.requestFlickrData(document.getElementById("search-text").value);
       Actions.requestInstaTagData(document.getElementById("search-text").value);
-      Actions.request4SquareData(document.getElementById("search-text").value);
     };
+    function getSuggestions(input, callback) {
+      //const regex = new RegExp('^' + input, 'i');
+      //const suggestions = places.filter(suburb => regex.test(suburb));
+      $.ajax({
+        url: "https://api.foursquare.com/v2/venues/search?near=" + input + "&client_id=DVIANEZN3RGP1LFGSJNTYYGZKBBSK5PEVMUHUXB0NBGVB5GA&client_secret=WG10HBEMTT5TK5IJOREQHPRGY5YGFZML22SY4D1SY21NY1WH&v=20151215",
+        jsonp: "callback",
+        dataType: "jsonp"
+      }).done(data => {
+        return callback(null, data.response.venues);
+      });
+
+    }
+    function renderSuggestion(suggestion, input) { // In this example, 'suggestion' is a string
+      return (                                     // and it returns a ReactElement
+          <span><strong>{suggestion.name.slice(0, input.length)}</strong>{suggestion.name.slice(input.length)}</span>
+      );
+    }
     return (
       <div className="search-component">
-        <input id="search-text" type="text" />
-        <button onClick={onSearch}>Search</button>
+        <div>
+          <input id="search-text" type="text" />
+          <button onClick={onSearch}>Search</button>
+        </div>
+        <div>
+          <Autosuggest suggestions={getSuggestions}
+                       suggestionRenderer={renderSuggestion}
+                       suggestionValue={item => item.name + ' (' + item.stats.checkinsCount + ')'}
+                       showWhen={input => input.trim().length >= 5} />
+        </div>
       </div>
     );
   }
