bb1d3b5699c3a320a559cf2a771fa3812bcb88e4
akonovalov <akonovalov@at-consulting.ru>
2015-12-15 23:03:55 +0300

    add 4square requests

--- src/actions/Action.js
+++ src/actions/Action.js
@@ -1,27 +1,35 @@
 import dispatcher from '../dispatcher/Dispatcher';
 
 export default class Actions {
-    static navigate(newRoute) {
-        dispatcher.dispatch('NAVIGATE', { location: newRoute });
-    }
+  static navigate(newRoute) {
+    dispatcher.dispatch('NAVIGATE', { location: newRoute });
+  }
 
-    static requestInstaData(lat, lng) {
-        dispatcher.dispatch('REQUEST-INSTA-DATA', { lat: lat, lng: lng });
-    }
+  static requestInstaData(lat, lng) {
+    dispatcher.dispatch('REQUEST-INSTA-DATA', { lat: lat, lng: lng });
+  }
 
-    static requestInstaTagData(tag) {
-        dispatcher.dispatch('REQUEST-INSTA-TAG-DATA', { tag: tag });
-    }
+  static requestInstaTagData(tag) {
+    dispatcher.dispatch('REQUEST-INSTA-TAG-DATA', { tag: tag });
+  }
 
-    static processInstaData(data) {
-        dispatcher.dispatch('PROCESS-INSTA-DATA', data);
-    }
+  static processInstaData(data) {
+    dispatcher.dispatch('PROCESS-INSTA-DATA', data);
+  }
 
-    static requestFlickrData(tag) {
-        dispatcher.dispatch('REQUEST-FLICKR-DATA', { tag: tag });
-    }
+  static requestFlickrData(tag) {
+    dispatcher.dispatch('REQUEST-FLICKR-DATA', { tag: tag });
+  }
 
-    static processFlickrData(data) {
-        dispatcher.dispatch('PROCESS-FLICKR-DATA', data);
-    }
+  static processFlickrData(data) {
+    dispatcher.dispatch('PROCESS-FLICKR-DATA', data);
+  }
+
+  static request4SquareData(tag) {
+    dispatcher.dispatch('REQUEST-4SQUARE-DATA', { tag: tag });
+  }
+
+  static process4SquareData(data) {
+    dispatcher.dispatch('PROCESS-4SQUARE-DATA', data);
+  }
 }
--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -6,89 +6,104 @@ import $ from 'jquery';
 
 class AppStore extends Store {
 
-    constructor() {
-        super('AppStore');
-        this.logger.debug('Initializing AppStore');
+  constructor() {
+    super('AppStore');
+    this.logger.debug('Initializing AppStore');
 
-        this.initialize('pages', [
-            { name: 'welcome', title: 'Welcome', nav: true, auth: false, default: true },
-            { name: 'flickr', title: 'Flickr', nav: true, auth: false },
-            { name: 'spells', title: 'Spells', nav: true, auth: true }
-        ]);
-        this.initialize('route', this.getNavigationRoute(window.location.hash.substr(1)));
-        this.initialize('images', []);
-        this.initialize('instaData', []);
-        this.initialize('lastFlickrRequest', 0);
-        this.initialize('lastInstaTagRequest', 0);
-    }
+    this.initialize('pages', [
+      { name: 'welcome', title: 'Welcome', nav: true, auth: false, default: true },
+      { name: 'flickr', title: 'Flickr', nav: true, auth: false },
+      { name: 'spells', title: 'Spells', nav: true, auth: true }
+    ]);
+    this.initialize('route', this.getNavigationRoute(window.location.hash.substr(1)));
+    this.initialize('images', []);
+    this.initialize('venues', []);
+    this.initialize('instaData', []);
+    this.initialize('lastFlickrRequest', 0);
+    this.initialize('lastInstaTagRequest', 0);
+  }
 
-    onAction(actionType, data) {
-        this.logger.debug(`Received Action ${actionType} with data`, data);
-        switch (actionType) {
+  onAction(actionType, data) {
+    this.logger.debug(`Received Action ${actionType} with data`, data);
+    switch (actionType) {
 
-            case 'NAVIGATE':
-                let newRoute = this.getNavigationRoute(data.location);
-                if (newRoute !== this.get('route')) {
-                    this.set('route', newRoute);
-                    window.location.hash = `#${newRoute}`;
-                }
-                break;
-
-            case 'REQUEST-FLICKR-DATA':
-                let lastRequest = this.get('lastFlickrRequest');
-                let currentTime = Date.now;
-                let fiveMinutes = 5 * 60 * 1000;
-                if ((currentTime - lastRequest) > fiveMinutes) {
-                    return;
-                }
-                $.ajax({
-                    url: 'http://api.flickr.com/services/feeds/photos_public.gne',
-                    data: { tags: data.tag, tagmode: 'any', format: 'json' },
-                    dataType: 'jsonp',
-                    jsonp: 'jsoncallback'
-                }).done(response => {
-                    Actions.processFlickrData(response);
-                });
-                break;
-
-            case 'PROCESS-FLICKR-DATA':
-                this.set('images', data.items);
-                break;
-
-            case 'REQUEST-INSTA-TAG-DATA':
-                let lastInstaRequest = this.get('lastInstaTagRequest');
-                let currentInstaTime = Date.now;
-                let fiveInstaMinutes = 5 * 60 * 1000;
-                if ((currentInstaTime - lastInstaRequest) > fiveInstaMinutes) {
-                    return;
-                }
-                $.ajax({
-                    //url: "https://api.instagram.com/v1/media/search?lat=48.858093&lng=2.294694&client_id=e050a30d1667451cbc3598f3cce20530",
-                    url: "https://api.instagram.com/v1/tags/" + data.tag + "/media/recent?client_id=e050a30d1667451cbc3598f3cce20530",
-                    jsonp: "callback",
-                    dataType: "jsonp"
-                }).done(response => {
-                    Actions.processInstaData(response);
-                });
-                break;
-
-            case 'PROCESS-INSTA-DATA':
-                this.set('instaData', data.data);
-                break;
-
-            default:
-                this.logger.debug('Unknown actionType for this store - ignoring');
-                break;
+      case 'NAVIGATE':
+        let newRoute = this.getNavigationRoute(data.location);
+        if (newRoute !== this.get('route')) {
+          this.set('route', newRoute);
+          window.location.hash = `#${newRoute}`;
         }
-    }
+        break;
 
-    getNavigationRoute(route) {
-        let newRoute = find(this.get('pages'), path => { return path.name === route.toLowerCase(); });
-        if (!newRoute) {
-            newRoute = find(this.get('pages'), path => { return path.default && path.default === true; });
+      case 'REQUEST-FLICKR-DATA':
+        let lastRequest = this.get('lastFlickrRequest');
+        let currentTime = Date.now;
+        let fiveMinutes = 5 * 60 * 1000;
+        if ((currentTime - lastRequest) > fiveMinutes) {
+          return;
         }
-        return newRoute.name || '';
+        $.ajax({
+          url: 'http://api.flickr.com/services/feeds/photos_public.gne',
+          data: { tags: data.tag, tagmode: 'any', format: 'json' },
+          dataType: 'jsonp',
+          jsonp: 'jsoncallback'
+        }).done(response => {
+          Actions.processFlickrData(response);
+        });
+        break;
+
+      case 'PROCESS-FLICKR-DATA':
+        this.set('images', data.items);
+        break;
+
+      case 'REQUEST-INSTA-TAG-DATA':
+        let lastInstaRequest = this.get('lastInstaTagRequest');
+        let currentInstaTime = Date.now;
+        let fiveInstaMinutes = 5 * 60 * 1000;
+        if ((currentInstaTime - lastInstaRequest) > fiveInstaMinutes) {
+          return;
+        }
+        $.ajax({
+          //url: "https://api.instagram.com/v1/media/search?lat=48.858093&lng=2.294694&client_id=e050a30d1667451cbc3598f3cce20530",
+          url: "https://api.instagram.com/v1/tags/" + data.tag + "/media/recent?client_id=e050a30d1667451cbc3598f3cce20530",
+          jsonp: "callback",
+          dataType: "jsonp"
+        }).done(response => {
+          Actions.processInstaData(response);
+        });
+        break;
+
+      case 'PROCESS-INSTA-DATA':
+        this.set('instaData', data.data);
+        break;
+
+      case 'REQUEST-4SQUARE-DATA':
+        $.ajax({
+          url: "https://api.foursquare.com/v2/venues/search?near=" + data.tag + "&client_id=DVIANEZN3RGP1LFGSJNTYYGZKBBSK5PEVMUHUXB0NBGVB5GA&client_secret=WG10HBEMTT5TK5IJOREQHPRGY5YGFZML22SY4D1SY21NY1WH&v=20151215",
+          jsonp: "callback",
+          dataType: "jsonp"
+        }).done(response => {
+          Actions.process4SquareData(response);
+        });
+        break;
+
+      case 'PROCESS-4SQUARE-DATA':
+        this.set('venues', data.response.venues);
+        break;
+
+      default:
+        this.logger.debug('Unknown actionType for this store - ignoring');
+        break;
     }
+  }
+
+  getNavigationRoute(route) {
+    let newRoute = find(this.get('pages'), path => { return path.name === route.toLowerCase(); });
+    if (!newRoute) {
+      newRoute = find(this.get('pages'), path => { return path.default && path.default === true; });
+    }
+    return newRoute.name || '';
+  }
 }
 
 var appStore = new AppStore();
