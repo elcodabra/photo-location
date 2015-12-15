8a017c891d4bb95581972728cb41a02e862be930
akonovalov <akonovalov@at-consulting.ru>
2015-12-15 23:21:45 +0300

    add 4square requests

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -2,15 +2,35 @@
 
 import React from 'react';
 import Actions from '../actions/Action';
+import appStore from '../stores/AppStore';
 import * as $ from 'jquery';
 
 require('styles//Search.less');
 
 class SearchComponent extends React.Component {
+
+  componentWillMount() {
+    this.appStoreId = appStore.registerView(() => { this.updateState(); });
+    this.updateState();
+  }
+
+  componentWillUnmount() {
+    appStore.deregisterView(this.appStoreId);
+  }
+
+  updateState() {
+    this.setState({
+      places: appStore.get('venues')
+    });
+  }
+
   render() {
+    let places = this.state.places;
+    console.log(places);
     var onSearch = function() {
       //Actions.requestFlickrData(document.getElementById("search-text").value);
       Actions.requestInstaTagData(document.getElementById("search-text").value);
+      Actions.request4SquareData(document.getElementById("search-text").value);
     };
     return (
       <div className="search-component">
