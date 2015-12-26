8bd397236e294a098fb3c195e10a71a0f6a3a70e
Alex Konovalov <akonovalov@at-consulting.ru>
2015-12-26 15:03:13 +0300

    change production build

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 /*eslint no-console:0 */
+var express = require('express');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
+var open = require('open');
 var config = require('./webpack.config');
+var compiler = webpack(config);
+var app = express();
 
-new WebpackDevServer(webpack(config), config.devServer)
-.listen(config.port, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
-  //open('http://localhost:' + config.port + '/webpack-dev-server/');
-});
+//console.log(compiler);
+
+//process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var isDev = process.env.NODE_ENV === 'development';
+
+if (isDev) {
+  new WebpackDevServer(compiler, config.devServer)
+    .listen(config.port, 'localhost', function(err) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('Listening at localhost:' + config.port);
+      open('http://localhost:' + config.port + '/webpack-dev-server/');
+    });
+} else {
+  app.set('port', (process.env.PORT || config.port));
+  app.use("/", express.static(__dirname + '/dist/'));
+  app.listen(app.get('port'));
+  console.log('Listening at ' + __dirname + '/dist/' + ' port:' + app.get('port'));
+}
+
