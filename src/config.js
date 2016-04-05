5e721dfb5ee794599341376c4f34e1c60f295cac
sani-banani <a.konovalov88@gmail.com>
2016-04-05 14:53:52 +0300

    Delete config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "instagram_client_id": "e050a30d1667451cbc3598f3cce20530",
-  "foursquare_client_id": "DVIANEZN3RGP1LFGSJNTYYGZKBBSK5PEVMUHUXB0NBGVB5GA",
-  "foursquare_client_secret": "WG10HBEMTT5TK5IJOREQHPRGY5YGFZML22SY4D1SY21NY1WH"
-}
