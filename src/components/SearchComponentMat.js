a5a061c0da25f20884fbf71da526af44ad9383bf
Alex Konovalov <akonovalov@at-consulting.ru>
2016-04-08 16:22:25 +0300

    change to menu item

--- src/components/SearchComponentMat.js
+++ src/components/SearchComponentMat.js
@@ -4,14 +4,12 @@ import React from 'react';
 import Actions from '../actions/Action';
 import appStore from '../stores/AppStore';
 import AutoComplete from 'material-ui/lib/auto-complete';
-import MenuItem from 'material-ui/lib/menus/menu-item';
 import MapsPlace from 'material-ui/lib/svg-icons/maps/place';
 import Search from 'material-ui/lib/svg-icons/action/search';
-import Check from 'material-ui/lib/svg-icons/navigation/check';
 import Avatar from 'material-ui/lib/avatar';
 import Colors from 'material-ui/lib/styles/colors';
-import List from 'material-ui/lib/lists/list';
-import ListItem from 'material-ui/lib/lists/list-item';
+//import ListItem from 'material-ui/lib/lists/list-item';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 
 require('styles//SearchMaterial.less');
 
@@ -28,26 +26,33 @@ class SearchComponentMat extends React.Component {
 
   updateState() {
     this.setState({
-      places: appStore.get('venues').map( (item) => {
+      places: appStore.get('venues').map( (item, index) => {
+        let props = {};
+        if ( item.location_id )
+          props.leftIcon = <MapsPlace color={Colors.grey500} backgroundColor={Colors.transparent}/>;
+        else
+          props.leftAvatar = <Avatar color={Colors.grey500} backgroundColor={Colors.transparent} style={{left: 10, top: 5}}>#</Avatar>; //&#9839;
         return {
           text: item.name,
           value: (
-            /*<MenuItem
-              leftIcon={ item.location_id ? <MapsPlace/> : '#' } //&#9839;
-              //rightIcon= { <Check/> }
+            <MenuItem
+              //ref={`selectElement${index}`}
+              {...props}
               primaryText={ item.name }
-              secondaryText={(<b>{item.media_count}</b>)}
-            />*/
-            // FIXME: add selectable list - http://www.material-ui.com/#/components/list
-            <ListItem
-              leftIcon={ item.location_id ? <MapsPlace/> : '' }
-              leftAvatar={ !item.location_id ? <Avatar color={Colors.grey500} backgroundColor={Colors.transparent} style={{left: 10}}>#</Avatar> : '' } //&#9839;
-              primaryText={ item.name }
-              secondaryText={(<b>{item.media_count}</b>)}
-              //rightToggle={(<b>{item.media_count}</b>)}
-              value={ item }
-              onMouseEnter={ (e) => { console.log(e); console.log(item); console.log(this.value) } }
+              onTouchTap={ () => this.newRequest(item) }
+              innerDivStyle={{ paddingLeft: '55px' }}
             />
+            /*
+            <ListItem
+              {...props}
+              primaryText={ item.name }
+              disableKeyboardFocus={true}
+              //secondaryText={(<b>{item.media_count}</b>)}
+              onTouchTap={ () => this.newRequest(item) }
+              innerDivStyle={{ paddingLeft: '55px' }}
+              onKeyboardFocus={ (e) => { console.log(e) } }
+            />
+            */
           )
         }
       }),
@@ -71,12 +76,10 @@ class SearchComponentMat extends React.Component {
   };
 
   newRequest = (value) => {
-    var venue = _.filter(this.state.venues, {name: value})[0];
-
-    if (!venue.location_id) {
-      Actions.requestInstaTagData(venue.name);
+    if (!value.location_id) {
+      Actions.requestInstaTagData(value.name);
     } else {
-      Actions.requestPlacesLocation({ place_id: venue.location_id });
+      Actions.requestPlacesLocation({ place_id: value.location_id });
     }
   };
 
@@ -84,13 +87,14 @@ class SearchComponentMat extends React.Component {
     return (
       <div className="search-component-mat">
         <AutoComplete
-          //floatingLabelText="Type and select location or tag"
-          hintText="Type and select location or tag"
+          floatingLabelText="Type and select location or tag"
+          //hintText="Type and select location or tag"
           animated={true}
           dataSource={this.state.places}
           filter={ AutoComplete.noFilter /*(searchText, key) => searchText !== '' && key.toLowerCase().includes(searchText.toLowerCase())*/ }
           onUpdateInput={this.handleUpdateInput}
-          onNewRequest={this.newRequest}
+          //onKeyUp={(e) => { console.log(this.refs); }}
+          //onNewRequest={this.newRequest}
           style={{ width: '500px' }}
           listStyle={{ width: '500px', textAlign: 'left' }}
           maxSearchResults={5}
